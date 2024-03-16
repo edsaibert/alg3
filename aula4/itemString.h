bd728aef773298442c6fcee5437fc4ade6e1d5da
edsaibert <eduardasaibert@ufpr.br>
2024-03-16 11:54:11 -0300

    [COMPLETO] introducao itemString.h

---
+++ aula4/itemString.h
@@ -0,0 +1,11 @@
+
+#include <string.h>
+
+#define cp(A, B) (strcpy(A, B))
+#define lt(A, B) (strcmp(A, B) < 0)
+#define gt(A, B) (strcmp(A, B) > 0)
+#define eq(A, B) (strcmp(A, B) == 0)
+
+#define read(A) (scanf("%s", A));
+#define write(A) (printf("%s", A));
+
