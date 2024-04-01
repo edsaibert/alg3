12516c6c494a413031b6a3cbf9e5a4c4b8ed4896
edsaibert <eduarda6saibert@gmail.com>
2024-04-01 13:10:22 -0300

    [COMPLETO] funcao somaChave

---
+++ trab1/README.md
@@ -0,0 +1,3 @@
+
+### Comando de execução
+gcc -Wall -g clienteArvBin.c -lm tadArvBinInt.o tadFilaApontador.o -o main
--- trab1/clienteArvBin.c
+++ trab1/clienteArvBin.c
@@ -13,29 +13,35 @@ typedef int ItemArv;
 /* Funções */
 
 /*  Calcula a soma de todas as chaves da arvore */
-int somaChave( arvBin arv ){};
+int somaChave ( ArvBin arv ){
+  if (arv == NULL)
+    return 0;
+  
+  return (arv->item + somaChave(arv->esq) + somaChave(arv->dir));
+
+};
 
 /*  Calcula o valor mınimo das chaves armazenadas na arvore. Caso a arvore esteja vazia o valor retornado
     deve ser zero 
 */
-int valorMinimo( arvBin arv ){};
+int valorMinimo( ArvBin arv ){};
 
 /*  Altera a arvore de forma que na arvore final a chave do nodo pai e igual a chave do seu maior filho */
-arvBin paiMaior( arvBin arv ){};
+ArvBin paiMaior( ArvBin arv ){};
 
 /*  Alterar os valores das chaves dos nodos de forma que a maior chave da arvore fique na raiz. A arvore deve
     manter as chaves originais, fazendo a troca do valor de um nodo com um dos seus filhos caso seu valor nao
     seja o maior  
 */
-arvBin maiorNaRaiz( arvBin arv ){};
+ArvBin maiorNaRaiz( ArvBin arv ){};
 
 /*  funcao que altera uma arvore de forma que cada nodo
     contém o maior valor dentre os seus filhos (mantendo todas as chaves originais da arvore)
 */
-arvBin ordenaPeloMaior( arvBin arv ){};
+ArvBin ordenaPeloMaior( ArvBin arv ){};
 
 /*  Dobra a quantidade de nodos da arvore criando para cada um novo pai */
-arvBin dobraArvore( arvBin arv ){};
+ArvBin dobraArvore( ArvBin arv ){};
 
 
 /* ----------------------------------------------------- */
@@ -55,9 +61,9 @@ int main(int argc, char *argv[]){
   printf( "\nArvore\n" );
   escreveArv( arv );
 
-/*----   chamada das funcoes da lista ----
-  printf("%d\n", somaChave(...));
-
+/*----   chamada das funcoes da lista ----*/
+  printf("%d\n", somaChave(arv));
+/*
   printf("%d\n" valorMinimo(...) );
 
   ... paiMaior(...)
